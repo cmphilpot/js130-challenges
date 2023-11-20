07437569b977b1007af69c19096a018b5f81a519
Chase Philpot <philpotchase@gmail.com>
2023-11-20 09:55:25 -0500

    Completed dna challenge

--- dna.js
+++ dna.js
@@ -1,15 +1,88 @@
-class DNA {};
+/*
+Notes:
+Write a program that can calculate the Hamming distance between two DNA strands.
+The Hamming distance is only defined for sequences of equal length.
+If you have two sequences of unequal length, you should compute the
+Hamming distance over the shorter length.
 
+*/
 
+class DNA {
+  constructor (matchStrand) {
+    this.matchStrand = matchStrand;
+  }
 
+  hammingDistance(compareStrand) {
+    let copyOfMatch = this.matchStrand.slice();
+    let copyOfCompare = compareStrand.slice();
 
+    if (copyOfCompare === copyOfMatch) return 0;
+    if (copyOfMatch.length > copyOfCompare.length) copyOfMatch = copyOfMatch.slice(0, copyOfCompare.length);
+    if (copyOfCompare.length > copyOfMatch.length) copyOfCompare = copyOfCompare.slice(0, copyOfMatch.length);
 
+    let counter = 0;
 
+    for (let index = 0; index < copyOfMatch.length; index += 1) {
+      if (copyOfMatch[index] !== copyOfCompare[index]) counter += 1;
+    }
 
+    return counter;
+  }
+};
 
+let one = new DNA('');
+console.log(one.hammingDistance('')); // 0
 
+let two = new DNA('GGACTGA');
+console.log(two.hammingDistance('GGACTGA')); // 0
 
+let three = new DNA('ACT');
+console.log(three.hammingDistance('GGA')); // 3
 
+let strand = 'GGACGGATTCTGACCTGGACTAATTTTGGGG';
+let distance = 'AGGACGGATTCTGACCTGGACTAATTTTGGGG';
 
+let four = new DNA(strand);
+console.log(four.hammingDistance(distance)); // 19
+
+let five = new DNA('GGACG');
+console.log(five.hammingDistance('GGTCG')); // 1
+
+let six = new DNA('ACCAGGG');
+console.log(six.hammingDistance('ACTATGG')); // 2
+
+let seven = new DNA('AAACTAGGGG');
+console.log(seven.hammingDistance('AGGCTAGCGGTAGGAC')); // 3
+
+let eight = new DNA('GACTACGGACAGGGTAGGGAAT');
+let distance2 = 'GACATCGCACACC';
+console.log(eight.hammingDistance(distance2)); // 5
+
+let nine = new DNA('AGACAACAGCCAGCCGCCGGATT');
+console.log(nine.hammingDistance('AGGCAA')); // 1
+console.log(nine.hammingDistance('AGACATCTTTCAGCCGCCGGATTAGGCAA')); // 4
+console.log(nine.hammingDistance('AGG')); // 1
 
 module.exports = DNA;
+
+/*
+
+
+
+
+*/
+
+/*
+tests
+
+1. "no difference between empty strands"
+2. "no difference between identical strands"
+3. "complete hamming distance in small strand"
+4. "hamming distance in off by one strand"
+5. "small hamming distance in middle somewhere"
+6. "larger distance"
+7. "ignores extra length on other strand when longer"
+8. "ignores extra length on original strand when longer"
+9. "does not actually shorten original strand"
+
+*/
--- sumOfMultiples.js
+++ sumOfMultiples.js
@@ -1,7 +1,67 @@
-class SumOfMultiples {};
+class SumOfMultiples {
+  constructor (...setOfNumbers) {
+    this.setOfNumbers = setOfNumbers;
+  }
 
+  to(number) {
+    // less than number, sum All multiples
+    let arrOfMultiples = this.setOfNumbers.map(element => this.getMultiples(element, number)).flat(1);
 
+    console.log(arrOfMultiples);
 
+    return arrOfMultiples.reduce((acc, curr) => acc += curr, 0);
+  }
+
+  getMultiples(element, number) {
+    let multiples = [];
+    let incrementingElement = element;
+
+    while (element < number) {
+      multiples.push(incrementingElement);
+      incrementingElement += element;
+    }
+
+    return multiples;
+  }
+
+  static to(number) {
+    let arr = SumOfMultiples.getMultiples(number);
+
+    return arr.reduce((acc, curr) => acc += curr, 0);
+  }
+
+  static getMultiples(number) {
+    let multiples = [];
+    let counter = 1;
+
+    while (counter < number) {
+      if ((counter % 3 === 0) || (counter % 5 === 0)) {
+        multiples.push(counter);
+      }
+      counter += 1;
+    }
+
+    return multiples;
+  }
+};
+
+// console.log(SumOfMultiples.to(1)); // 0
+// console.log(SumOfMultiples.to(4)); // 3
+// console.log(SumOfMultiples.to(10)); // 23
+// console.log(SumOfMultiples.to(100)); // 2_318
+// console.log(SumOfMultiples.to(1000)); // 233_168
+
+// let first = new SumOfMultiples(7, 13, 17);
+// console.log(first.to(20)); // 51
+
+// let second = new SumOfMultiples(4, 6);
+// console.log(second.to(15)); // 30
+
+let third = new SumOfMultiples(5, 6, 8)
+console.log(third.to(150)); // 4419
+
+// let fourth = new SumOfMultiples(43, 47)
+// console.log(fourth.to(10000)); // 2203160
 
 
 
@@ -13,3 +73,26 @@ class SumOfMultiples {};
 
 
 module.exports = SumOfMultiples;
+
+/*
+tests
+
+"sum to one"
+"sum to three"
+"sum to ten"
+"sum to one hundred"
+"sum to one thousand"
+"configurable 7 13 17 to 20" [7, 13, 17, 20]
+"configurable 4 6 to 15" [4, 6, 15]
+"configurable 5 6 8 to 150" [5, 6, 8]
+"configurable 43 47 to 10,000" [43, 47]
+
+SumOfMultiples.to(1); 0
+SumOfMultiples.to(4); // 3
+SumOfMultiples.to(10); // 23
+SumOfMultiples.to(100); // 2_318
+SumOfMultiples.to(1000); // 233_168
+
+
+
+*/
