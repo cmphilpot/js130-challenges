1e8f0c111f461477639fe89c6ebc516926b2de0e
Chase Philpot <philpotchase@gmail.com>
2023-12-09 15:10:53 -0500

    completed custom set problem

--- custom-set.js
+++ custom-set.js
@@ -1,19 +1,9 @@
 'use strict';
 
-/*
-combinedArray.indexOf(element) === combinedArray.lastIndexOf(element));
-  return returnVal.filter((el, idx, arr) => arr.indexOf(el) === idx);
-
-     this.filteredForUniques = array.filter((element, idx, arr) => arr.indexOf(element));
-    console.log(this.set);
-*/
-
 class CustomSet {
   constructor (array) {
     this.LEN = Array.isArray(array) ? array.length : 0;
-    this.array = array;
-    // this.filteredForUniques = array.filter((element, idx, arr) => arr.indexOf(element));
-    console.log(this.set);
+    this.array = Array.isArray(array) ? array : [];
   }
 
   isEmpty() {
@@ -38,7 +28,6 @@ class CustomSet {
 
   isDisjoint(newObject) {
     if (this.isEmpty() || newObject.isEmpty()) return true;
-
     return newObject.array.every(el => !this.array.includes(el));
   }
 
@@ -46,74 +35,109 @@ class CustomSet {
     return (newObject.LEN === this.LEN) && (newObject.array.every(el => this.array.includes(el)));
   }
 
-  add () {
-
+  add (newValue) {
+    if (this.isEmpty()) return new CustomSet([newValue])
+    if (!this.isEmpty()) {
+      let combined = [...this.array, newValue].filter((element, index, array) => array.indexOf(element) === index);
+      return new CustomSet(combined)
+    }
   }
 
-  intersection () {
+  intersection (newObject) {
+    if (this.isEmpty() || newObject.isEmpty()) return new CustomSet();
+    if (!this.array.some(element => newObject.array.includes(element))) return new CustomSet();
 
+    let shared = [];
+
+    for (let index = 0; index < this.LEN; index += 1) {
+      if (newObject.array.includes(this.array[index])) {
+        shared.push(this.array[index]);
+      }
+    }
+
+    return new CustomSet(shared);
   }
 
-  difference () {
-
+  difference (newObject) {
+    if (this.isEmpty()) return new CustomSet();
+    if (!this.isEmpty() && newObject.isEmpty()) return new CustomSet([...this.array]);  // <<
+    if (!this.isEmpty() && !newObject.isEmpty()) {
+      return new CustomSet([...this.array.filter(ele => !newObject.array.includes(ele))]);  // <<
+    }
   }
 
-  union () {
+  union (newObject) {
+    if (this.isEmpty() && newObject.isEmpty()) return new CustomSet();
+    if (this.isEmpty() && !newObject.isEmpty()) return new CustomSet(newObject.array);
+    if (!this.isEmpty() && newObject.isEmpty()) return new CustomSet(this.array);
+    if (!this.isEmpty() && !newObject.isEmpty()) {
+      let combined = [...newObject.array, ...this.array]
+      .filter((element, index, array) => array.indexOf(element) === index);
+      return new CustomSet(combined);
+    }
 
   }
 }
 
 module.exports = CustomSet;
 
-// let empty = new CustomSet();
-// console.log(empty.isEmpty()); // true
+/*
+combinedArray.indexOf(element) === combinedArray.lastIndexOf(element));
+  return returnVal.filter((el, idx, arr) => arr.indexOf(el) === idx);
 
-// let one = new CustomSet([1]);
-// console.log(one.isEmpty()); // false
+     this.filteredForUniques = array.filter((element, idx, arr) => arr.indexOf(element));
+    console.log(this.set);
+*/
+/* tests
+let empty = new CustomSet();
+console.log(empty.isEmpty()); // true
 
-// let containsOne = new CustomSet();
-// console.log(containsOne.contains(1)); // false
+let one = new CustomSet([1]);
+console.log(one.isEmpty()); // false
 
-// let firstGroup = new CustomSet([1, 2, 3]);
-// console.log(firstGroup.contains(1)); // true
+let containsOne = new CustomSet();
+console.log(containsOne.contains(1)); // false
 
-// let secondGroup = new CustomSet([1, 2, 3]);
-// console.log(secondGroup.contains(4));       //  false
+let firstGroup = new CustomSet([1, 2, 3]);
+console.log(firstGroup.contains(1)); // true
 
-// let subset1 = new CustomSet();
-// console.log(subset1.isSubset(new CustomSet())); // true
+let secondGroup = new CustomSet([1, 2, 3]);
+console.log(secondGroup.contains(4));       //  false
 
-// let subset2 = new CustomSet();
-// console.log(subset2.isSubset(new CustomSet([1]))); // true
+let subset1 = new CustomSet();
+console.log(subset1.isSubset(new CustomSet())); // true
 
-// let subset3 = new CustomSet([1]);
-// console.log(subset3.isSubset(new CustomSet())); // false
+let subset2 = new CustomSet();
+console.log(subset2.isSubset(new CustomSet([1]))); // true
 
-// let subset4 = new CustomSet([1, 2, 3]);
-// console.log(subset4.isSubset(new CustomSet([1, 2, 3]))); // true
+let subset3 = new CustomSet([1]);
+console.log(subset3.isSubset(new CustomSet())); // false
 
-// let subset5 = new CustomSet([1, 2, 3]);
-// console.log(subset5.isSubset(new CustomSet([4, 1, 2, 3]))); // true
+let subset4 = new CustomSet([1, 2, 3]);
+console.log(subset4.isSubset(new CustomSet([1, 2, 3]))); // true
 
-// let subset6 = new CustomSet([1, 2, 3]);
-// console.log(subset6.isSubset(new CustomSet([4, 1, 3]))); // false
+let subset5 = new CustomSet([1, 2, 3]);
+console.log(subset5.isSubset(new CustomSet([4, 1, 2, 3]))); // true
 
-// last <> "set is not a subset of set that does not contain its elements"
+let subset6 = new CustomSet([1, 2, 3]);
+console.log(subset6.isSubset(new CustomSet([4, 1, 3]))); // false
 
-// let disjoint1 = new CustomSet();
-// console.log(disjoint1.isDisjoint(new CustomSet([]))) // true
+last <> "set is not a subset of set that does not contain its elements"
 
-// let disjoint2 = new CustomSet();
-// console.log(disjoint2.isDisjoint(new CustomSet([1]))) // true
+let disjoint1 = new CustomSet();
+console.log(disjoint1.isDisjoint(new CustomSet([]))) // true
 
-// let disjoint3 = new CustomSet([1]);
-// console.log(disjoint3.isDisjoint(new CustomSet([]))) // true
+let disjoint2 = new CustomSet();
+console.log(disjoint2.isDisjoint(new CustomSet([1]))) // true
 
-// let disjoint4 = new CustomSet([1, 2]);
-// console.log(disjoint4.isDisjoint(new CustomSet([2, 3]))) // false
+let disjoint3 = new CustomSet([1]);
+console.log(disjoint3.isDisjoint(new CustomSet([]))) // true
 
-// let disjoint5 = new CustomSet([1, 2]);
-// console.log(disjoint5.isDisjoint(new CustomSet([3, 4]))) // true
+let disjoint4 = new CustomSet([1, 2]);
+console.log(disjoint4.isDisjoint(new CustomSet([2, 3]))) // false
+
+let disjoint5 = new CustomSet([1, 2]);
+console.log(disjoint5.isDisjoint(new CustomSet([3, 4]))) // true
 
 let same1 = new CustomSet();
 console.log(same1.isSame(new CustomSet())); // trye
@@ -126,3 +150,42 @@ console.log(same3.isSame(new CustomSet())); // false
 
 let same4 = new CustomSet([1, 2]);
 console.log(same4.isSame(new CustomSet([2, 1])));
+
+const actual = new CustomSet();
+actual.add(3);
+console.log(actual);              // [3]
+
+const actual2 = new CustomSet([1, 2, 4]);
+actual2.add(3);
+console.log(actual2);          // [1, 2, 4, 3]
+
+const actual3 = new CustomSet([1, 2, 3]);
+actual3.add(3);
+console.log(actual3);      // [1, 2, 3]
+
+const actual = new CustomSet();
+console.log(actual.intersection(new CustomSet()));
+
+const actual2 = new CustomSet();
+console.log(actual2.intersection(new CustomSet([3, 5, 7])));
+// const comparison = new CustomSet();
+
+const actual3 = new CustomSet([3, 5, 7]);
+console.log(actual3.intersection(new CustomSet()));
+// const comparison = new CustomSet();
+
+"intersection of two sets with no shared elements is an empty set"
+const actual = new CustomSet([1, 2, 3]);
+console.log(actual.intersection(new CustomSet([4, 5, 6])));  // []
+
+"intersection of two sets with shared elements is a set of the shared elements"
+const actual1 = new CustomSet([1, 2, 3, 4]);
+console.log(actual1.intersection(new CustomSet([3, 2, 5]))); // [2, 3]
+
+const actual = new CustomSet([1, 2, 3, 4]);
+console.log(actual.difference(new CustomSet())); // [1, 2, 3, 4]
+
+
+const actual1 = new CustomSet([3, 2, 1]);
+console.log(actual1.difference(new CustomSet([2, 4])));  // [3, 1]
+*/
